ca22c73b5e2f0577821b1a9309f09e3b08b30d56
Andreas <silenta237@gmail.com>
2015-11-11 22:24:30 -0800

    Added BartApi default constructor Added class vars to BartApi: baseURL, key, stations Added class: routeinfo to handle the routeinfo API call

--- app/src/main/java/com/csueastbay/jialiang/testapp/BartApi.java
+++ app/src/main/java/com/csueastbay/jialiang/testapp/BartApi.java
@@ -3,6 +3,7 @@ package com.csueastbay.jialiang.testapp;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
@@ -24,6 +25,36 @@ import org.xml.sax.SAXException;
  * Created by Jialiang on 10/13/2015.
  */
 public class BartApi {
+    // must add command and options
+    String baseURL = "http://api.bart.gov/api/sched.aspx?cmd=" ;
+    // API key
+    String key = "key=MW9S-E7SL-26DU-VV8V&b=2&a=2&l=1" ;
+    // Station abbreviations list
+    List<String> stations = new ArrayList<>();
+
+    // Default constructor adds stations to the list
+    BartApi() {
+        stations.add("DALY"); // Daly City
+        stations.add("BALB"); // Balboa
+        stations.add("GLEN"); // Glen Park
+        stations.add("24TH"); // 24th st
+        stations.add("16TH"); // 16th st
+        stations.add("CIVC"); // Civic center
+        stations.add("POWL"); // Powell
+        stations.add("MONT"); // Montgomery
+        stations.add("EMBR"); // Embarcadero
+        stations.add("WOAK"); // West Oakland
+        stations.add("LAKE"); // Lake Merritt
+        stations.add("FTVL"); // Fruitvale
+        stations.add("COLS"); // Coliseum
+        stations.add("SANL"); // San Leandro
+        stations.add("BAYF"); // Bayfaire
+        stations.add("HAYW"); // Hayward
+        stations.add("SHAY"); // South Hayward
+        stations.add("UCTY"); // Union City
+        stations.add("FRMT"); // Fremont
+    }// end Default BartApi()
+
 
     private static InputStream doGet(String url) throws ClientProtocolException, IOException {
         HttpClient httpClient = new DefaultHttpClient();
@@ -34,6 +65,24 @@ public class BartApi {
         return inputStream;
     }
 
+
+    /*
+    Var: String newBaseURL - add the command and flags to the baseURL inherited from BartApi baseURL
+    Var: String routeinfo_command - routeinfo command string
+    Var: String routeinfo_route
+     */
+    public class routeinfo {
+
+        String newBaseURL; // will have the command and flags added
+        String routeinfo_command = "routeinfo" ;
+        String routeinfo_route = "route" ;
+
+        routeinfo(){ newBaseURL = baseURL + routeinfo_command; }
+
+
+
+        }
+    }// end public class routeinfo
     public String returnString() throws ClientProtocolException, IOException, ParserConfigurationException, SAXException {
 
         String contentString;
